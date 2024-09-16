8c5f6f9538c1da8589345e4b0d596c31cfd676e9
Harsh Sharma <52374089+29-Harsh@users.noreply.github.com>
2024-09-15 22:17:33 -0400

    Adding more details to scenario , step Definition and feature files
    This commit includes various types of writing scenario while using parameterization.
    Also, adding regex to accept any data type in step definition.

--- src/test/java/features/firstFeature.feature
+++ src/test/java/features/firstFeature.feature
@@ -7,8 +7,39 @@
 Feature: Application login
   This feature file is to test login process of sauce labs webiste.
 
-  Scenario: Admin login
-    Given Admin is on login page
-    When Admin enters credentials
-    Then Display Home page
 
+#Scenario: Admin login
+#  Given Admin is on login page
+#  When Admin enters credentials
+#  Then Display Home page
+
+#while you have same type of scenario for diff data
+#for e.g wehave n user to login, so instead of writing multiple scenario, use scenario outline where we can paramatrize
+
+#Parameterization in cucumber scenario
+#Use this when you want to run same testcase with different data sets having same behaviour
+Scenario Outline: User login
+  Given Admin is on login page
+  When Admin enters credentials username "<User>" and password "<Pass>"
+  Then Display Home page
+  
+#if you want to pass any other data type alphanumeric paramter 
+#use regex  
+  
+ Examples:
+ 	|User|Pass|
+ 	|CreditUser|1234|
+ 	|DebitUser|2341| 
+ 	
+ 	
+#In case you want to fill up form, so it's not good idea to pass multiple dat in single line. 
+#When Admin enters credentials username "<User>" and password "<Pass>"
+#Use this when you want to pass multiple data in same step.
+Scenario: User signup
+  Given User is on signup page
+  When User enters credentials
+	|filling|
+	|the |
+	|Signup|
+	|page|
+ Then Display Home page
--- src/test/java/stepDefinitions/MainSteps.java
+++ src/test/java/stepDefinitions/MainSteps.java
@@ -1,5 +1,49 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class MainSteps {
 
+	@Given("Admin is on login page")
+	public void admin_is_on_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("Admin is on login page"); 
+	    //throw new io.cucumber.java.PendingException();
+	}
+	
+	
+	//@When("^Admin enters credentials username (.+) and password (.+) $")
+	// This is for regex to accept any type of data from feature file "ANYTHING" - IMPORTANT
+	
+	
+	@When("Admin enters credentials username {string} and password {string}")
+	public void admin_enters_credentials_username_and_password(String a, String b) {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println(a+ "admin_enters_credentials_username_and_password" + b); 
+	    //throw new io.cucumber.java.PendingException();
+	}
+	
+	
+	/*  This is use when you have list of data for e.g filling form
+	 * @When("User enters credentials")
+	public void user_enters_credentials(io.cucumber.datatable.DataTable dataTable) {
+    // Write code here that turns the phrase above into concrete actions
+    // For automatic transformation, change DataTable to one of
+    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+    //
+    // For other transformations you can register a DataTableType.
+    throw new io.cucumber.java.PendingException();
+}*/
+	
+	@Then("Display Home page")
+	public void display_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("display_home_page"); 
+	    //throw new io.cucumber.java.PendingException();
+	}
+	
 }
